f1427edef64d7902f86820565ce3cc030d3d754e
Mathias Schöben <mathias@Mathiass-MacBook-Pro.local>
2019-06-11 18:52:12 +0200

    battery signals to hud missing

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -5,7 +5,7 @@ var count = 0
 func _ready():
 	pass # Replace with function body.
 
-func _on_Coin_body_entered(body):
+func _on_Battery_body_entered(body):
 	if body.get_name() == "Player":
 		count += 1
 		$Control/CoinCount.text = String(count)
