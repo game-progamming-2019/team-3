21bc629cafe1091eaf922c680923d942c62baedc
LukasMaus <49515857+LukasMaus@users.noreply.github.com>
2019-06-12 15:18:28 +0200

    Add files via upload

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,12 +1,7 @@
 extends CanvasLayer
 
-var count = 0
-
 func _ready():
 	pass # Replace with function body.
 
-func _on_Battery_body_entered(body):
-	if body.get_name() == "Player":
-		count += 1
-		$Control/CoinCount.text = String(count)
-	pass # Replace with function body.
+func _process(delta):
+	$Control/CoinCount.text = String(global.battery_count)
