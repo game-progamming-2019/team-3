b50388bd22535baf50901cfed9fea1374bd81bf6
Troy <tose.esnu@gmail.com>
2019-06-12 11:26:17 +0200

    Added collisions to enemies

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -12,6 +12,9 @@ var direction : Vector2 = Vector2(1,0)
 
 var json
 
+onready var collision = $Collision
+onready var sprite = $Sprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var file = File.new()
@@ -20,24 +23,20 @@ func _ready():
 	createEnemy()
 
 
-
 func createEnemy():
-	$Sprite.texture = load("res://Enemy/Sprites/" + json["enemies"][id]["Texture"][0]["File"])
-	$Sprite.region_enabled  = true
-	$Sprite.region_rect.position.x = json["enemies"][id]["Texture"][0]["region"][0]["x"]
-	$Sprite.region_rect.position.y = json["enemies"][id]["Texture"][0]["region"][0]["y"]
-	$Sprite.region_rect.size.x = json["enemies"][id]["Texture"][0]["region"][0]["w"]
-	$Sprite.region_rect.size.y = json["enemies"][id]["Texture"][0]["region"][0]["h"]
-	$Sprite.scale = Vector2(2.0,2.0)
-	$Sprite.position = get_parent().position 
+	sprite.texture = load("res://Enemy/Sprites/" + json["enemies"][id]["Texture"]["File"])
+	sprite.region_enabled  = true
+	sprite.region_rect.position.x = json["enemies"][id]["Texture"]["region"]["x"]
+	sprite.region_rect.position.y = json["enemies"][id]["Texture"]["region"]["y"]
+	sprite.region_rect.size.x = json["enemies"][id]["Texture"]["region"]["w"]
+	sprite.region_rect.size.y = json["enemies"][id]["Texture"]["region"]["h"]
+
+	collision.shape.extents.x = json["enemies"][id]["Collision"]["size"]["x"]
+	collision.shape.extents.y = json["enemies"][id]["Collision"]["size"]["y"]
+	
+	self.scale = Vector2(2, 2)
 	
 	if(json["enemies"][id]["Movement"] == "linear"):
-		#var scene = load("res://LinearMovement.tscn")
-		#add_child(scene.instance())
 		add_child(LinearMovement.new())
 	elif(json["enemies"][id]["Movement"] == "sine"):
 		add_child(SineMovement.new())
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
