47df7a81d7530d9e73fed97087b6647ce0e246b8
mattionline <github@mattionline.de>
2019-07-25 13:06:10 +0200

    Slingshot pulling fixed

--- scenes/Slingshot.gd
+++ scenes/Slingshot.gd
@@ -3,16 +3,17 @@ extends Node2D
 var bird
 
 func _process(delta):
-	update_elastics($ElasticBack)
-	update_elastics($ElasticFront)
+	update_elastic($ElasticBack)
+	update_elastic($ElasticFront)
 	
-func update_elastics(elastic):
+func update_elastic(elastic):
 	var attach_pos = self.bird.get_node("AttachPoint").get_global_position() if self.bird else $LaunchPoint.get_global_position()
 	var diff_pos = attach_pos - elastic.get_node("FixationPoint").get_global_position()
 	var middle = diff_pos / 2
 	var sprite = elastic.get_node("Sprite")
 	sprite.position = middle
-	sprite.scale.x = middle.length()
+	sprite.scale.x = - middle.length() * 0.01
+	sprite.rotation = middle.angle()
 	
 func attach_bird(bird):
 	self.bird = bird
