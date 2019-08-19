73233aa08c88c7da8cdde61864364f9eb4eddc9f
LukasMaus <49515857+LukasMaus@users.noreply.github.com>
2019-08-19 17:47:32 +0200

    Delete Level.gd

--- team-3-master/levels/Level.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# Level testen - Szene - Rechtsklick - Diese Szene abspielen
-
-# Bird an Slingshot festbinden
-# Darf nur einen Bird geben
-# Name muss Birds -> Bird sein
-func _ready():
-	$Birds/Bird.attach_to($Slingshot)
