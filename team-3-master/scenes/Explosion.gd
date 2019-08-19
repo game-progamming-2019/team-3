51789eb04c3cd338f0d6719a8cd342c03830f781
LukasMaus <49515857+LukasMaus@users.noreply.github.com>
2019-08-19 17:45:12 +0200

    Delete Explosion.gd

--- team-3-master/scenes/Explosion.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite
-
-func _ready():
-	# Animation abspielen
-	$AnimationPlayer.play("default")
-	# Signal finished senden
-	yield($AnimationPlayer, "animation_finished");
-	# Animation beenden
-	queue_free()
